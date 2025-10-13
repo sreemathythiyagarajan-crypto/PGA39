e4e5ba1afc5379273c61548761d5c5e714a6c58c
sreemathythiyagarajan-crypto <sreemathythiyagarajan@gmail.com>
2025-10-13 09:37:27 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,37 @@
+import pickle
+from flask import Flask, request
+
+app = Flask(_name_)
+
+def model_infrence(data):
+    with open('model.pkl','rb') as models:
+        pred_model = pickle.load(models)
+
+    predictions = pred_model.predict(data)
+
+    return pedictions
+
+@app.route('/',methods = ['POST'])
+def funcs():
+    return 'Serrver is UP'
+
+@app.route('/predictions',methods = ['POST'])
+def preds():
+    s1 = eval(request.form[''])
+    sw = eval(request.form[''])
+    p1 = eval(request.form[''])
+    pw = eval(request.form[''])
+
+    if model == 'Logistic Regression':
+        model = 'model'
+    elif model == 'Random Forest':
+        model = 'rf_model'
+    else:pass
+
+    data = [[s1,sw,p1,pw]]
+
+    predicted_values = model_inference(datas,model)
+
+    return f'Predicted Value: {predicted_values},Used Model{model}'
+
+app.run(
